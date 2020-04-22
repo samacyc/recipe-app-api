4ae3ba81f2d8aeff1017001b63f86b6a815bd403
samacyc <ismailecom28@gmail.com>
2020-04-22 13:50:04 +0000

    adding permission

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -8,6 +8,7 @@ from rest_framework import status
 
 CREATE_USER_URL = reverse('user:create')
 TOKEN_URL = reverse('user:token')
+ME_URL =reverse('user:me')
 
 
 def create_user(**params) :
@@ -90,3 +91,37 @@ class PublicUserApiTests(TestCase) :
         res = self.client.post(TOKEN_URL , payload)
         self.assertIn('password' , res.data)
         self.assertEqual(res.status_code , status.HTTP_400_BAD_REQUEST)
+    def test_retrieve_unauthorized(self) :
+        #Test that authentication required
+        res = self.client.get(ME_URL)
+        self.assertEqual(res.status_code , 401)
+
+class PrivateAPITests(TestCase) :
+    def setUp(self) :
+        self.user = create_user(
+        email = 'test@samacyc.com',
+        password = 'egfdgbj' ,
+        name = 'smacyc'        )
+        self.client = APIClient()
+        self.client.force_authenticate(user =self.user)
+    def test_retrieve_profile_success(self) :
+        # Test Ret
+        res = self.client.get(ME_URL)
+        self.assertEqual(res.status_code , 200)
+        self.assertEqual(res.data , {
+            'name' : self.user.name ,
+            'email' : self.user.email
+        })
+    def test_post_me_not_allowed(self) :
+        res = self.client.post(ME_URL , {})
+        self.assertEqual(res.status_code , status.HTTP_405_METHOD_NOT_ALLOWED)
+    def test_updating_user_profile(self) :
+        payload = {
+            'name' : 'test@samacyc.com',
+            'password' : 'worngpass'
+        }
+        res = self.client.patch(ME_URL , payload)
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.name , payload['name'])
+        self.assertTrue(self.user.check_password(payload['password']))
+        self.assertEqual(res.status_code , status.HTTP_200_OK)
